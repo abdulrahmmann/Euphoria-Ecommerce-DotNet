8fe24694f8f2d472d3a8cb8296d3075ee7c4e69c
Abdulrahman <abd3lra7man.078@gmail.com>
2025-11-18 00:54:16 +0300

    Implement Size CRUD methods and XML docs.

--- EuphoriaCommerce.Domain/Entities/CatalogDomain/Size.cs
+++ EuphoriaCommerce.Domain/Entities/CatalogDomain/Size.cs
@@ -3,17 +3,33 @@ using EuphoriaCommerce.Domain.Entities.ProductDomain;
 
 namespace EuphoriaCommerce.Domain.Entities.CatalogDomain;
 
-/// <summary> Represents a product size. </summary>
+/// <summary>
+/// Represents a product size.
+/// </summary>
 public class Size: Entity<Guid>
 {
+    /// <summary>
+    /// The size name.
+    /// <example>Small, </example> <example>38</example>
+    /// </summary>
     public string Name { get; private set; } = null!;
+    
+    /// <summary>
+    /// The classification type of the size.
+    /// <example>Clothing, </example> <example>Shoes</example>
+    /// </summary>
     public string SizeType { get; private set; } = null!;
     
     public ICollection<ProductVariant> Variants { get; private set; } = new List<ProductVariant>();
     
     private Size() { }
 
-    private Size(string name, string sizeType)
+    #region Constructor | Create
+    /// <summary>Constructor | Create a Size and mark as Created.</summary>
+    /// <param name="name">The Size Name.</param>
+    /// <param name="sizeType">The Size Type.</param>
+    /// <param name="createdBy">Admin Who create the Size.</param>
+    private Size(string name, string sizeType, string? createdBy = null)
     {
         ArgumentException.ThrowIfNullOrEmpty(name);
         ArgumentException.ThrowIfNullOrEmpty(sizeType);
@@ -22,5 +38,39 @@ public class Size: Entity<Guid>
         
         Name = name;
         SizeType = sizeType;
+        
+        MarkCreated(createdBy);
     }
+    #endregion
+
+    #region Helper Methods : Update, Delete, Restore
+    /// <summary>Update a Size and mark as Modified.</summary>
+    /// <param name="name">The Size Name.</param>
+    /// <param name="sizeType">The Size Type.</param>
+    /// <param name="modifiedBy">Admin Who modify the Size.</param>
+    public void Update(string name, string sizeType, string? modifiedBy = null)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentException.ThrowIfNullOrEmpty(sizeType);
+
+        Name = name;
+        SizeType = sizeType;
+
+        MarkModified(modifiedBy);
+    }
+    
+    /// <summary> Softly Delete a Size and mark as Deleted.</summary>/ 
+    /// <param name="deletedBy">Admin Who delete the size.</param>
+    public void Delete(string? deletedBy = null)
+    {
+        MarkDeleted(deletedBy); 
+    }
+    /// <summary> Restore Softly Deleted Size and mark as Restored.</summary>/ 
+    /// <param name="restoredBy">the admin who restore the size.</param>
+    public void Restore(string? restoredBy = null)
+    {
+        MarkRestored(restoredBy);
+    }
+    #endregion
+    
 }
