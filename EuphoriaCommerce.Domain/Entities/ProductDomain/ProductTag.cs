301d88322212350ddd918c7628287aa5ddd0dc92
Abdulrahman <abd3lra7man.078@gmail.com>
2025-11-18 01:00:50 +0300

    Implement ProductTag CRUD methods and XML docs.

--- EuphoriaCommerce.Domain/Entities/ProductDomain/ProductTag.cs
+++ EuphoriaCommerce.Domain/Entities/ProductDomain/ProductTag.cs
@@ -2,19 +2,60 @@
 
 namespace EuphoriaCommerce.Domain.Entities.ProductDomain;
 
-/// <summary> Represent a Product Tag. </summary>
-public class ProductTag: Entity<Guid>
+/// <summary>
+/// Represents a product tag.
+/// <example>New Arrival, Best Seller, Trending</example>
+/// </summary>
+public class ProductTag : Entity<Guid>
 {
+    /// <summary>The tag name.</summary>
     public string Name { get; private set; } = null!;
-    
+
+    /// <summary>Navigation property to products with this tag.</summary>
     public ICollection<Product> Products { get; private set; } = new List<Product>();
 
-    private ProductTag() {}
+    private ProductTag() { }
 
-    public ProductTag(string name)
+    #region Constructor | Create
+    /// <summary>Constructor | Create a ProductTag and mark as Created.</summary>
+    /// <param name="name">Tag name.</param>
+    /// <param name="createdBy">Admin who created the tag.</param>
+    public ProductTag(string name, string? createdBy = null)
     {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+
         Id = Guid.NewGuid();
-        
         Name = name;
+
+        MarkCreated(createdBy);
     }
+    #endregion
+
+    #region Helper Methods : Update, Delete, Restore
+    /// <summary>Update a ProductTag and mark as Modified.</summary>
+    /// <param name="name">Tag name.</param>
+    /// <param name="modifiedBy">Admin who modified the tag.</param>
+    public void Update(string name, string? modifiedBy = null)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+
+        Name = name;
+
+        MarkModified(modifiedBy);
+    }
+
+    /// <summary>Softly delete a ProductTag and mark as Deleted.</summary>
+    /// <param name="deletedBy">Admin who deleted the tag.</param>
+    public void Delete(string? deletedBy = null)
+    {
+        MarkDeleted(deletedBy);
+    }
+
+    /// <summary>Restore a softly deleted ProductTag and mark as Restored.</summary>
+    /// <param name="restoredBy">Admin who restored the tag.</param>
+    public void Restore(string? restoredBy = null)
+    {
+        MarkRestored(restoredBy);
+    }
+    #endregion
 }
